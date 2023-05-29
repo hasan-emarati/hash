1b10e423977dc3745afa48b5c5cbd23ce99f982b
SHemarati <hasan.emarati.12652@gmail.com>
2023-05-29 16:15:29 +0330

    click and command line

--- Test/test.py
+++ Test/test.py
@@ -1,24 +1,31 @@
-import click
+# import click
 
-@click.group()
-def cli():
-    pass
+# @click.group()
+# def cli():
+#     pass
 
-@click.command()
-def function1():
-    # Code for function 1
-    click.echo('Function 1 is running')
+# @click.command()
+# def function1():
+#     # Code for function 1
+#     click.echo('Function 1 is running')
 
-@click.command()
-def function2():
-    # Code for function 2
-    click.echo('Function 2 is running')
+# @click.command()
+# def function2():
+#     # Code for function 2
+#     click.echo('Function 2 is running')
 
-cli.add_command(function1)
-cli.add_command(function2)
+# cli.add_command(function1)
+# cli.add_command(function2)
 
-if __name__ == '__main__':
-    cli()
+# if __name__ == '__main__':
+#     cli()
+
+
+
+
+
+
+
 
 
 # import click
@@ -39,3 +46,42 @@ if __name__ == '__main__':
 # if __name__ == '__main__':
 #     function1()
 #     function2()
+
+
+
+
+
+
+
+
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('text')
+def hash_search(text):
+    """
+    Search in hash.
+    """
+    click.echo(f"Searching text: {text}")
+
+@cli.command()
+def pass_to_hash():
+    """
+    Translate password list to hash.
+    """
+    click.echo("Translating password list to hash...")
+
+@cli.command()
+@click.argument('value')
+def set_value(value):
+    """
+    Set value.
+    """
+    click.echo(f"Value set to {value}.")
+
+if __name__ == '__main__':
+    cli()
--- Tools/main.py
+++ Tools/main.py
@@ -4,6 +4,9 @@ from tkinter.filedialog import askopenfile
 from tkinter import *
 
 class main:
+    @click.group()
+    def cli():
+        pass
     def __init__(self):
         print (Fore.YELLOW + '''
            ______  _    _           _     
@@ -15,14 +18,43 @@ class main:
                         
                       Created by SHemarati 
 ________________________________________________________________\n'''+ Fore.RESET)
-    def Search_In_Hash(self, text):
-        print("Searching")
+    @cli.command()
+    def IseeHash():
+        click.echo('search in hash is running')
+        print(f"Searching text: ")
 
-    def pass_List_To_Hash(self):
-        print("Passing")
+    @cli.command()
+    def P2H():
+        """
+        Translate password list to hash.
+        """
+        click.echo("Translating password list to hash...")
 
-    def Val_To_Hash(self, Val):
-        print("Val=")
+    @cli.command()
+    def value():
+        Val = input(Fore.LIGHTBLUE_EX + "Enter Your Value >>> ")
+        
 
+    cli.add_command(IseeHash)
+    cli.add_command(P2H)
+    cli.add_command(value)
+
+    def Protocol():
+        while True:
+            print(Fore.YELLOW + "Place Select Your Protocol You Have a Some Protocol : ")
+            print('''
+            [1] > sha256        SHA-256 is a cryptographic hashing algorithm that produces a 256-bit (32-byte) hash value for a given input, widely used for digital signatures and encryption.  
+            [2] > sha512        SHA-512 is a cryptographic hash function that produces a unique fixed-size output (512 bits) from an input data, commonly used for data integrity and security purposes.
+            [3] > sha3_256      SHA3-256 is a cryptographic hash function that takes an input data and produces a fixed-size output of 256 bits, providing secure hashing and data integrity verification for various applications.
+            [4] > md5           MD5 is a cryptographic hash function that produces a fixed-size output of 128 bits from an input data, but due to its vulnerabilities, it is no longer considered secure for cryptographic purposes.
+            [5] > sha1          SHA-1: A cryptographic hash function that produces a fixed-size output of 160 bits, but is no longer considered secure for cryptographic applications.
+            [6] > blake2s       BLAKE2s is a fast and secure cryptographic hash function that produces a fixed-size output of up to 256 bits, commonly used for data integrity and privacy-preserving protocols.
+            [7] > whirlpool
+            [8] > shake_128     SHAKE128 is a fast and secure cryptographic hash function that can produce a variable-length output of high quality, suitable for various applications such as key derivation and message authentication.
+            [9] > shake_256     SHAKE256 is a cryptographic hash function that produces a variable-length output of high quality, based on the SHA-3 family, and is suitable for various applications such as key derivation and digital signatures.
+            ''' + Fore.RESET)
+            Protocol = input(Fore.LIGHTBLUE_EX + "Enter Your Protocol Number >>> ")
+        
 if __name__ == '__main__':
     main()
+    main.cli()
